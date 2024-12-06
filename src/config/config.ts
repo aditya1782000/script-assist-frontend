56cc7c422b78aee73daaeb271435831fada232ab
aditya1782000 <adityathakkar178@gmail.com>
2024-12-06 23:56:27 +0530

    fix: api-url

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 const config = {
   appName: "Interview",
   // apiUrl: "http://localhost:5001/api/v1",
-  apiUrl: "https://9068-2405-201-2001-298b-e146-8c0b-7fd2-d8c0.ngrok-free.app/api/v1/"
+  apiUrl: "https://a59a-2405-201-2001-298b-e146-8c0b-7fd2-d8c0.ngrok-free.app/api/v1/"
 };
 
 export default config;
