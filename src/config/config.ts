de69e037a4f2df7e4c43e8c8894cc552c0577fa2
aditya1782000 <adityathakkar178@gmail.com>
2024-12-06 23:18:33 +0530

    Add: url

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 const config = {
   appName: "Interview",
   // apiUrl: "http://localhost:5001/api/v1",
-  apiUrl: "https://a78c-2405-201-2001-298b-e146-8c0b-7fd2-d8c0.ngrok-free.app/api/v1/"
+  apiUrl: "https://587d-2405-201-2001-298b-e146-8c0b-7fd2-d8c0.ngrok-free.app/api/v1/"
 };
 
 export default config;
