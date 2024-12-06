02b5a8dab1192a8e2fa857d195f2c5effdd1ef0a
aditya1782000 <adityathakkar178@gmail.com>
2024-12-06 23:25:46 +0530

    Add: url

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 const config = {
   appName: "Interview",
   // apiUrl: "http://localhost:5001/api/v1",
-  apiUrl: "https://192.168.29.198/api/v1/"
+  apiUrl: "https://192.168.29.198: 5001/api/v1/"
 };
 
 export default config;
